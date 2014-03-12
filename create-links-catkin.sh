863cc09b1996d779e8adb2398e4317fbc885031e
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-03-12 03:48:41 +0100

    Added create-links-catkin.sh script.

---
+++ create-links-catkin.sh
@@ -0,0 +1,7 @@
+ln -s /usr/local/opt/ros-hydro-catkin/setup.sh /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/setup.zsh /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/setup.bash /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/_setup_util.py /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/env.sh /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/.catkin /usr/local
+ln -s /usr/local/opt/ros-hydro-catkin/.rosinstall /usr/local
